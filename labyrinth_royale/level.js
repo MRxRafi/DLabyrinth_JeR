6f321fca582cd0c45c682646e0af145b2c438ea4
Rafael <38105703+MRxRafi@users.noreply.github.com>
2018-10-12 15:58:13 +0200

    Me había equivocado, subido el código correcto

--- labyrinth_royale/level.js
+++ labyrinth_royale/level.js
@@ -1,74 +1,110 @@
 DLabyrinth.levelState = function(game){
 
 }
-var drawnObject1, drawnObject2;
-var width, height, c1, c2;
-var vx, vy;
+var up, down, left, right; //Animacion
 
 DLabyrinth.levelState.prototype = {
     preload: function() {
-        
+        game.load.spritesheet('spriteSheet', 'assets/spriteSheets/spriteSheet1.png', 30, 49, 40);
+        game.load.image('orb', 'assets/props/orb.png');
+        game.load.image('bg', 'assets/tiles/debug.png');
     },
 
     create: function() {
+        
+        //Separación del orbe
+        this.orbOffsetX = 20;
+        this.orbOffestY = 20;
 
-        //Rectangulo
-        width = 75;
-        height = 75;
+        //Sprite background
+        this.bg = game.add.tileSprite(0, 0, 1920, 1920, 'bg');
 
-        c1 = this.game.add.bitmapData(width, height);
-        c1.ctx.beginPath();
-        c1.ctx.rect(0, 0, width, height);
-        c1.ctx.fillStyle = '#ffffff';
-        c1.ctx.fill();
+        //Límites del mundo para la cámara
+        game.world.setBounds(0, 0, 1920, 1920);
 
-        c2 = this.game.add.bitmapData(width, height);
-        c2.ctx.beginPath();
-        c2.ctx.rect(0, 0, width, height);
-        c2.ctx.fillStyle = '#00FFFF';
-        c2.ctx.fill();
-        drawnObject1 = game.add.sprite(game.world.centerX, game.world.centerY, c1);
-        drawnObject2 = game.add.sprite(game.world.centerX, game.world.centerY, c2);
-        //drawnObject.anchor.setTo(0.5, 0.5);
+        //Sprite del personaje y asignación de las animaciones a variables
+        this.player = game.add.sprite(300, 300, 'spriteSheet');
+        this.walkLeft = this.player.animations.add('walkLeft', [0, 1, 2, 3, 4, 5, 6, 7, 8]);
+        this.walkUp = this.player.animations.add('walkUp', [10, 11, 12, 13, 14, 15, 16, 17, 18]);
+        this.walkDown = this.player.animations.add('walkDown', [20, 21, 22, 23, 24, 25, 26, 27, 28]);
+        this.walkRight = this.player.animations.add('walkRight', [30, 31, 32, 33, 34, 35, 36, 37, 38]);
+        up = false; /* Estas variables sirven para administrar que animación debe ir y si está o no activada */
+        down = false;
+        left = false;
+        right = false;
+        //up = down = left = right = false;
+        this.player.animations.play('walkUp', 30, true);
 
-        //  Register the keys.
+        //Asignamos teclas a variables
         this.wKey = game.input.keyboard.addKey(Phaser.Keyboard.W);
         this.sKey = game.input.keyboard.addKey(Phaser.Keyboard.S);
         this.aKey = game.input.keyboard.addKey(Phaser.Keyboard.A);
         this.dKey = game.input.keyboard.addKey(Phaser.Keyboard.D);
+
+        //Añadimos orbe cerca del jugador
+        this.orb = game.add.sprite(this.player.x - this.orbOffsetX, this.player.y - this.orbOffsetY ,'orb');
+        this.orb.scale.setTo(0.05);
         
+        //Activamos físicas arcade para el orbe y le asignamos posición.
+        game.physics.enable(this.orb, Phaser.Physics.ARCADE);
+        this.orb.x = 300;
+        this.orb.y = 300;
+
+        //Activamos físicas arcade para el personaje
+        game.physics.enable(this.player, Phaser.Physics.ARCADE);
+
+        //La cámara sigue al jugador
+        game.camera.follow(this.player, Phaser.Camera.FOLLOW_LOCKON, 0.1, 0.1);
+       
     },
 
     update: function() {
-        vx = 0;
-        vy = 0;
-        if(this.wKey.isDown && vy == 0){
-            drawnObject1.visible = true;
-            vy = -1;
-            drawnObject2.visible = false;
-        }
-        if(this.sKey.isDown && vy == 0){
-            drawnObject1.visible = true;
-            vy = 1;
-            drawnObject2.visible = false;
-        }
-        if(this.aKey.isDown && vx == 0){
-            drawnObject1.visible = false;
-            vx = -1;
-            drawnObject2.visible = true;
-        }
-        if(this.dKey.isDown && vx == 0){
-            drawnObject1.visible = false;
-            vx = 1;
-            drawnObject2.visible = true;
-        }
-        drawnObject1.x += vx;
-        drawnObject1.y += vy;
-        drawnObject2.x += vx;
-        drawnObject2.y += vy;
-        //vx = 0;
-        //vy = 0;
-        
 
+        //Animación personaje
+        var keydown = false;
+        if (this.aKey.isDown){
+            //this.player.animations.play('walkLeft', 30, true)
+            this.player.x -= 5;
+            keydown = true;
+        } 
+        if (this.dKey.isDown){
+            //this.player.animations.play('walkRight', 30, true)
+            this.player.x += 5;
+            keydown = true;
+        }
+        if (this.wKey.isDown){
+            up = true;
+            //this.player.animations.play('walkUp', 30, true)
+            this.player.y -= 5;
+            keydown = true;
+        }
+        if (this.sKey.isDown){
+            down = true;
+            //this.player.animations.play('walkDown', 30, true)
+            this.player.y +=5;
+            keydown = true;
+        }
+        if(this.aKey.isDown && !this.wKey.isDown && !this.sKey.isDown) {
+            left = true;
+        }
+        if(this.dKey.isDown && !this.wKey.isDown && !this.sKey.isDown) {
+            right = true;
+        }
+
+        //Hacemos la animacion pertinente según lo obtenido anteriormente
+        if(up){ this.player.animations.play('walkUp', 30, true); }
+        if(down) { this.player.animations.play('walkDown', 30, true); }
+        if(left) { this.player.animations.play('walkLeft', 30, true); }
+        if(right) { this.player.animations.play('walkRight', 30, true); }
+        up = down = right = left = false;
+
+        //Paramos animación si no hay ninguna tecla pulsada
+        if(!keydown){ 
+            this.player.animations.stop(null, true);
+        }
+        this.orb.body.velocity.x = (this.player.x - this.orb.x - 30)*5;
+        this.orb.body.velocity.y = (this.player.y - this.orb.y - 30)*5;
+        
     }
 }
+
