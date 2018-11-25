d7376d10e39fd2c6cbffbe60af9c1d063d602d21
apozag <32688343+apozag@users.noreply.github.com>
2018-11-25 15:29:42 +0100

    items bueno

--- DLabyrinth_REST/src/main/java/es/urjc/ramstudios/dlabyrinth/Items.java
+++ DLabyrinth_REST/src/main/java/es/urjc/ramstudios/dlabyrinth/Items.java
@@ -1,27 +1,72 @@
 package es.urjc.ramstudios.dlabyrinth;
 
 public class Items {
-	private float ammo[][];
-	private float weapons[][];
+	private String [] weaponTypes;
+	private int [][] weaponPos;
+	
+	private String [] ammoTypes;
+	private int [][] ammoPos;
+	
+	private int [][] shieldPos;
+	
+	private int [][] foodPos;
+	
 	
 	public Items() {
-		
+		weaponTypes = new String [5];
+		weaponPos = new int [5][2];
+		ammoTypes = new String[6];
+		ammoPos = new int [5][2];
+		shieldPos = new int [4][2];
+		foodPos = new int [8][2];
 	}
-	public float[][] getAmmo() {
-		return ammo;
-	}
-
-	public void setAmmo(float[][] ammo) {
-		this.ammo = ammo;
-	}
-
-	public float[][] getWeapons() {
-		return weapons;
-	}
-
-	public void setWeapons(float[][] weapons) {
-		this.weapons = weapons;
-	}
-
 	
+	public String[] getWeaponTypes() {
+		return weaponTypes;
+	}
+
+	public void setWeaponTypes(String[] weaponTypes) {
+		this.weaponTypes = weaponTypes;
+	}
+
+	public int[][] getWeaponPos() {
+		return weaponPos;
+	}
+
+	public void setWeaponPos(int[][] weaponPos) {
+		this.weaponPos = weaponPos;
+	}
+
+	public String[] getAmmoTypes() {
+		return ammoTypes;
+	}
+
+	public void setAmmoTypes(String[] ammoTypes) {
+		this.ammoTypes = ammoTypes;
+	}
+
+	public int[][] getAmmoPos() {
+		return ammoPos;
+	}
+
+	public void setAmmoPos(int[][] ammoPos) {
+		this.ammoPos = ammoPos;
+	}
+
+	public int[][] getShieldPos() {
+		return shieldPos;
+	}
+
+	public void setShieldPos(int[][] shieldPos) {
+		this.shieldPos = shieldPos;
+	}
+
+	public int[][] getFoodPos() {
+		return foodPos;
+	}
+
+	public void setFoodPos(int[][] foodPos) {
+		this.foodPos = foodPos;
+	}
+
 }
