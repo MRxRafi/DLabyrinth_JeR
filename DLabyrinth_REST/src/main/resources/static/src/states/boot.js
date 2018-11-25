df9be8feb9fdccade82cfc3fc9c52f2a10f345a6
Astarhel <43136296+Astarhel@users.noreply.github.com>
2018-11-25 18:00:22 +0100

    subimos la fuente en el boot

--- DLabyrinth_REST/src/main/resources/static/src/states/boot.js
+++ DLabyrinth_REST/src/main/resources/static/src/states/boot.js
@@ -11,6 +11,8 @@ DLabyrinth.bootState.prototype = {
     preload: function(){
         game.physics.startSystem("Phaser.Physics.ARCADE");
         game.load.image('bar', 'assets/loading/bar.png');
+	//Cargamos la fuente antes de iniciar todo lo demas, si no no aparecerá cambiada en el preload    
+	game.load.script('webfont', '//ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js');
     },
 
     create: function(){
