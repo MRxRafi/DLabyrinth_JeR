19005330b1211cb15f5f1162d46dcfdd223835d9
apozag <32688343+apozag@users.noreply.github.com>
2018-12-11 21:39:50 +0100

    ahora si que si arreglado gameController

--- DLabyrinth_WS/src/main/java/es/urjc/ramstudios/dlabyrinth/GameController.java
+++ DLabyrinth_WS/src/main/java/es/urjc/ramstudios/dlabyrinth/GameController.java
@@ -1,72 +1,120 @@
 package es.urjc.ramstudios.dlabyrinth;
 
-public class Items {
-	private String [] weaponTypes;
-	private int [][] weaponPos;
+import java.util.Collection;
+import java.util.Map;
+//import java.util.Random;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+public class GameController {
+
+	Map<Long, Jugador> players = new ConcurrentHashMap<>();
+	AtomicLong nextId = new AtomicLong(0);
 	
-	private String [] ammoTypes;
-	private int [][] ammoPos;
+	TimeManager time = new TimeManager(); //Timer del mapa con su gestión de cierre de zonas
+	boolean startTimer = false;
 	
-	private int [][] shieldPos;
-	
-	private int [][] foodPos;
-	
-	
-	public Items() {
-		weaponTypes = new String [5];
-		weaponPos = new int [5][2];
-		ammoTypes = new String[6];
-		ammoPos = new int [5][2];
-		shieldPos = new int [4][2];
-		foodPos = new int [8][2];
+	Items items = new Items();
+
+	//COSAS DE JUGADOR
+	public Collection<Jugador> getPlayers() {
+		return players.values();
 	}
 	
-	public String[] getWeaponTypes() {
-		return weaponTypes;
+	public Jugador getPlayer(long id) {
+		return  players.get(id);
 	}
 
-	public void setWeaponTypes(String[] weaponTypes) {
-		this.weaponTypes = weaponTypes;
+	public Jugador newPlayer() {
+		Jugador player = new Jugador();
+		long id = nextId.incrementAndGet();
+		player.setId(id);
+		players.put(player.getId(), player);
+		return player;
 	}
 
+	public void deletePlayer(long id) {
+		Jugador savedPlayer = players.get(id);
+		if (savedPlayer != null) {
+			players.remove(savedPlayer.getId());
+		}
+	}
+	
+	public void updatePlayer(Jugador updPlayer) {
+		players.put(updPlayer.getId(), updPlayer);
+	}
+	
+	//COSAS DEL MAPA
+	public void startTimer() {
+		//Señal que inicia el timer. Debe enviarlo un solo jugador
+		time.start();
+		startTimer = true;
+	}
+	
+	public TimeManager getTimer() {
+		TimeManager savedTimer = time;
+		return savedTimer;
+	}
+	
+	public boolean getStartTimer() {
+		boolean strTimer = startTimer;
+		return strTimer;
+	}
+	
+	//ITEMS
+	public String[] getWeaponType() {
+		return items.getWeaponTypes();
+	}
+	
+	public void setWeaponType(String[] types) {
+		items.setWeaponTypes(types);
+	}
+	
 	public int[][] getWeaponPos() {
-		return weaponPos;
+		return items.getWeaponPos();
 	}
-
-	public void setWeaponPos(int[][] weaponPos) {
-		this.weaponPos = weaponPos;
-	}
-
-	public String[] getAmmoTypes() {
-		return ammoTypes;
-	}
-
-	public void setAmmoTypes(String[] ammoTypes) {
-		this.ammoTypes = ammoTypes;
-	}
-
-	public int[][] getAmmoPos() {
-		return ammoPos;
-	}
-
-	public void setAmmoPos(int[][] ammoPos) {
-		this.ammoPos = ammoPos;
-	}
-
-	public int[][] getShieldPos() {
-		return shieldPos;
-	}
-
-	public void setShieldPos(int[][] shieldPos) {
-		this.shieldPos = shieldPos;
-	}
-
-	public int[][] getFoodPos() {
-		return foodPos;
-	}
-
-	public void setFoodPos(int[][] foodPos) {
-		this.foodPos = foodPos;
-	}
-
 	
+	public void setWeaponPos(int[][] pos) {
+		items.setWeaponPos(pos);
+		//system.out.println()
+	}
+	
+	public String[] getAmmoType() {
+		return items.getAmmoTypes();
+	}
+
+	public void setAmmoType(String[] types) {
+		items.setAmmoTypes(types);
+	}
+	
+	public int[][] getAmmoPos() {
+		return items.getAmmoPos();
+	}
+	
+	public void setAmmoPos(int[][] pos) {
+		items.setAmmoPos(pos);
+	}
+	
+	public int[][] getShieldPos() {
+		return items.getShieldPos();
+	}
+
+	public void setShieldPos(int[][] pos) {
+		items.setShieldPos(pos);
+	}
+	
+	public int[][] getFoodPos() {
+		return items.getFoodPos();
+	}
+
+	public void setFoodPos(int[][] pos) {
+		items.setFoodPos(pos);
+	}
+
+}
